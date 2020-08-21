7a621db13db447be4770294fee2804be79869965
Nicolas Capelier <capelier.nicolas@gmail.com>
2020-08-21 23:23:31 +0200

    Imported Unity project

---
+++ Extra Credits Game Jam/Assets/Editor/UTPI/Build Packs/BuildPackEditor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace UTPI.BuildPacks
+{
+    [CustomEditor(typeof(BuildPack))]
+    public class BuildPackEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            BuildPack buildPack = (BuildPack)target;
+
+            GUILayout.Label("");
+
+            GUILayout.BeginHorizontal();
+
+            if(GUILayout.Button("Enable all scenes"))
+            {
+                buildPack.SetEnabling(true);
+            }
+
+            if (GUILayout.Button("Disable all scenes"))
+            {
+                buildPack.SetEnabling(false);
+            }
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.Label("");
+
+
+            /*if (GUILayout.Button("Get scenes from build settings"))
+            {
+                buildPack.GetBuildSettingsScenes();
+            }*/
+
+            if (GUILayout.Button("Place this pack in build settings"))
+            {
+                buildPack.SetToBuildSettings();
+            }
+
+        }
+    }
+
+}
---
+++ Extra Credits Game Jam/Assets/Packages/UTPI/Build Packs/BuildPack.cs
@@ -0,0 +1,51 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UTPI.BuildPacks
+{
+    [CreateAssetMenu(fileName = "new Build Pack", menuName ="Build Pack", order = 201)]
+    public class BuildPack : ScriptableObject
+    {
+        [System.Serializable]
+        public struct SceneToBuild
+        {
+            public SceneAsset sceneAsset;
+            public bool enabled;
+        }
+
+        public SceneToBuild[] scenesToBuild;
+
+        public void SetToBuildSettings()
+        {
+            EditorBuildSettingsScene[] newScenes = new EditorBuildSettingsScene[scenesToBuild.Length];
+
+            for (int i = 0; i < scenesToBuild.Length; i++)
+            {
+                string scenePath = AssetDatabase.GetAssetPath(scenesToBuild[i].sceneAsset);
+                newScenes[i] = new EditorBuildSettingsScene(scenePath, scenesToBuild[i].enabled);
+            }
+
+            EditorBuildSettings.scenes = newScenes;
+        }
+
+        public void GetBuildSettingsScenes()
+        {
+            scenesToBuild = new SceneToBuild[EditorBuildSettings.scenes.Length];
+
+            for (int i = 0; i < scenesToBuild.Length; i++)
+            {
+                scenesToBuild[i] = new SceneToBuild();
+                scenesToBuild[i].sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[i].path);
+                scenesToBuild[i].enabled = EditorBuildSettings.scenes[i].enabled;
+            }
+        }
+
+        public void SetEnabling(bool enabling)
+        {
+            for (int i = 0; i < scenesToBuild.Length; i++)
+            {
+                scenesToBuild[i].enabled = enabling;
+            }
+        }
+    }
+}
